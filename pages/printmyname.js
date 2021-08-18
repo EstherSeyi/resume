1ede70634bcba6b2570c3cde254fd98940a98e16
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-08-18 19:35:24 +0100

    updates print my name solution

--- pages/printmyname.js
+++ pages/printmyname.js
@@ -1,14 +1,4 @@
 import Head from "next/head";
-import Link from "next/link";
-import Image from "next/image";
-
-import HomeIcon from "../components/icons/home";
-import MailIcon from "../components/icons/mail";
-import PhoneIcon from "../components/icons/phone";
-import GlobeIcon from "../components/icons/globe";
-import CaretRight from "../components/icons/caret-right";
-
-import zuriLogo from "../assets/zuri-logo.svg";
 
 export default function Home() {
   const printName = (name) => {
@@ -19,11 +9,11 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Esther Ogundijo's Resume</title>
+        <title>Print Name</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="min-h-screen  relative z-10 border-b border-grey">
-        <p>My Name is {printName("Esther")}</p>
+      <main className="min-h-screen  relative z-10 border-b border-grey flex items-center justify-center">
+        <p>My Name is {printName("Esther")}.</p>
       </main>
       <footer className="text-center text-xs py-2 sticky right-0 left-0 bottom-0 ">
         Created with ❤️ by Esther Ogundijo at{" "}
